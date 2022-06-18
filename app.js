acae33303b7e6aa752ad0f658ff6bc476f144e60
mar1n <30738216+mar1n@users.noreply.github.com>
2022-06-18 15:45:24 +0100

    create routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
   db = require("./model/db"),
   routes = require("./routes"),
   user = require("./routes/user"),
+  project = require("./routes/project"),
   http = require("http"),
   path = require("path");
 
@@ -26,7 +27,31 @@ app.configure("development", function () {
 });
 
 app.get("/", routes.index);
-app.get("/users", user.list);
+
+app.get("/users", user.index);
+app.get("/user/new", user.create);
+app.post("/user/new", user.doCreate);
+app.get("/user/edit", user.edit);
+app.post('/user/edit', user.doEdit);
+app.get('/user/delete', user.confirmDelete);
+
+app.post('/user/delete', user.doDelete);
+
+app.get('/login', user.login);
+app.post('/login', user.doLogin);
+app.get('/logout', user.doLogout);
+
+app.get('/project/new', project.create);
+app.post('/project/new', project.doCreate);
+
+app.get('/project/:id', project.displayInfo);
+app.get('/project/edit/:id', project.edit);
+app.post('/project/edit/:id', project.doEdit);
+app.get('/project/delete/:id', project.confirmDelete);
+app.get('/project/delete/:id', project.doDelete);
+
+
+
 
 http.createServer(app).listen(app.get("port"), function () {
   console.log("Express server listening on port " + app.get("port"));
